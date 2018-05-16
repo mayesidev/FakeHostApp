1056e8fca15771df7ea1c17c68288bc71f2f2c79
Isaac Mayes <github@mayesid.com>
2018-05-15 21:35:26 -0500

    Implement FakeHostApp.

--- FakeHostApp/Program.cs
+++ FakeHostApp/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace FakeHostApp
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			Console.Title = ConfigurationManager.AppSettings["AppTitle"];
+			Console.SetWindowSize(30, 2);
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
 	}
 }
